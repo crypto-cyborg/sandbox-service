215154918f3b09febaca8afa020f0fecc94e7c2c
tng <m.farm4nov@gmail.com>
2024-11-30 23:57:01 +0400

    fix: account entity not saving

--- SandboxService.Application/Commands/CreateAccount/CreateAccountQuery.cs
+++ SandboxService.Application/Commands/CreateAccount/CreateAccountQuery.cs
@@ -1,12 +1,11 @@
 ﻿using LanguageExt.Common;
 using MediatR;
-using SandboxService.Core.Extensions;
 using SandboxService.Core.Models;
 
 namespace SandboxService.Application.Commands.CreateAccount;
 
-public class CreateAccountQuery(Guid walletId, int currencyId) : IRequest<Result<Account>>
+public class CreateAccountQuery(Guid walletId, string ticker) : IRequest<Result<Account>>
 {
     public Guid WalletId { get; } = walletId;
-    public int CurrencyId { get; } = currencyId;
+    public string Ticker { get; } = ticker;
 }
--- SandboxService.Application/Commands/CreateAccount/CreateAccountQueryHandler.cs
+++ SandboxService.Application/Commands/CreateAccount/CreateAccountQueryHandler.cs
@@ -12,22 +12,23 @@ public class CreateAccountQueryHandler(UnitOfWork unitOfWork) : IRequestHandler<
     public async Task<Result<Account>> Handle(CreateAccountQuery request, CancellationToken cancellationToken)
     {
         var wallet = await unitOfWork.WalletRepository.GetByIdAsync(request.WalletId, includeProperties: "Accounts");
-        var currency = await unitOfWork.CurrencyRepository.GetByIdAsync(request.CurrencyId);
+        var currency = await unitOfWork.CurrencyRepository.GetByTickerAsync(request.Ticker);
 
         if (wallet is null)
         {
             return new Result<Account>(new SandboxException("Wallet does not exist",
                 SandboxExceptionType.ENTITY_NOT_FOUND));
         }
-        
+
         if (currency is null)
         {
-            return new Result<Account>(new SandboxException("Currency does not exist",
-                SandboxExceptionType.ENTITY_NOT_FOUND));
+            currency = CurrencyExtensions.Create(request.Ticker, request.Ticker);
+            await unitOfWork.CurrencyRepository.InsertAsync(currency);
         }
 
         var account = AccountExtensions.Create(wallet.Id, currency.Id, 0);
-        
+        await unitOfWork.AccountRepository.InsertAsync(account);
+
         wallet.Accounts.Add(account);
         await unitOfWork.SaveAsync();
 
