9b11e4f992d41adecd6146c450bebb11e62b8878
Фарманов Магомед Альбертович <Farma_gi04@itstep.edu.az>
2024-12-10 14:49:08 +0400

    2ecc960 migrations

---
+++ SandboxService.Persistence/Migrations/20241210084211_SeparatingOrders.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SandboxService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeparatingOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_MarginPositions_PositionId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "StopLoss",
+                table: "MarginPositions");
+
+            migrationBuilder.DropColumn(
+                name: "TakeProfit",
+                table: "MarginPositions");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PositionId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLong",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_MarginPositions_PositionId",
+                table: "Orders",
+                column: "PositionId",
+                principalTable: "MarginPositions",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_MarginPositions_PositionId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsLong",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PositionId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "StopLoss",
+                table: "MarginPositions",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TakeProfit",
+                table: "MarginPositions",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_MarginPositions_PositionId",
+                table: "Orders",
+                column: "PositionId",
+                principalTable: "MarginPositions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
