2ecc9605abe173eed192e3c8ee05f894ba096a3d
Фарманов Магомед Альбертович <Farma_gi04@itstep.edu.az>
2024-12-10 14:46:10 +0400

    feat: updated models to separate orders from positions

--- SandboxService.Core/Extensions/OrderExtensions.cs
+++ SandboxService.Core/Extensions/OrderExtensions.cs
@@ -6,29 +6,33 @@ public static class OrderExtensions
 {
     public record OrderReadDto(
         Guid Id,
-        Guid PositionId,
+        Guid? PositionId,
         string Status,
         string Type,
         string Symbol,
         decimal Amount,
+        decimal Price,
+        bool IsLong,
         DateTimeOffset CreatedAt,
         DateTimeOffset? CompletedAt);
 
-    public static Order Create(Guid positionId, OrderType orderType, string symbol, decimal amount, Guid userId)
+    public static Order Create(OrderType orderType, string symbol, decimal amount, decimal price, bool isLong, int currencyId, Guid userId)
     {
         return new Order
         {
-            PositionId = positionId,
             Type = orderType,
             Symbol = symbol,
             Amount = amount,
+            Price = price,
+            IsLong = isLong,
+            CurrencyId = currencyId,
             UserId = userId
         };
     }
 
     public static OrderReadDto MapToResponse(this Order order)
         => new OrderReadDto(order.Id, order.PositionId, order.Status.ToString(), order.Type.ToString(),
-            order.Symbol, order.Amount, order.CreatedAt, order.CompletedAt);
+            order.Symbol, order.Amount, order.Price, order.IsLong, order.CreatedAt, order.CompletedAt);
 
     public static IEnumerable<OrderReadDto> MapToResponse(this IEnumerable<Order> orders)
         => orders.Select(o => o.MapToResponse());
